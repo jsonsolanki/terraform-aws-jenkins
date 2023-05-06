ca667b3357667e8c4b371ac8c046f382f0307117
Cloud User <centos@ip-172-31-18-26.us-west-1.compute.internal>
2023-05-06 06:40:00 +0000

    Welcome to JsonSolankiSimplicity

--- 01-Terraform-EC2/EC2/backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-  bucket = "bits-log-observ"
-  region = "us-west-1"
-  key = "test/terraform.tfstate"
-  }
-}
--- 01-Terraform-EC2/EC2/key.tf
+++
@@ -1,4 +0,0 @@
-data "aws_key_pair" "key" {
-    key_pair_id = var.key_id
-
-}
